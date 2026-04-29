384c06aa9280a6a09a21320b017d24f36d7fd1ec
DanielRotnitzky <drotnitzky@gmail.com>
2026-04-29 15:45:25 -0300

    feat: add profiles and configuration for local environment in WebConfig

--- backend/src/main/java/com/surfchamp/config/WebConfig.java
+++ backend/src/main/java/com/surfchamp/config/WebConfig.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile("local")
 public class WebConfig implements WebMvcConfigurer {
 
     @Bean
