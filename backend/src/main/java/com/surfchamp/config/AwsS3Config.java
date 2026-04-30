6924d2f99fec6e3bab112c465aa22815b8cc8298
DanielRotnitzky <drotnitzky@gmail.com>
2026-04-30 10:16:14 -0300

    Ajuste Database

--- backend/src/main/java/com/surfchamp/config/AwsS3Config.java
+++ backend/src/main/java/com/surfchamp/config/AwsS3Config.java
@@ -1,66 +1,49 @@
 package com.surfchamp.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+// import statements...
 
 @Configuration
-@Profile("!local")
+// @Profile("!local") // Comentado para desabilitar completamente
 public class AwsS3Config {
 
-    @Value("${aws.access.key.id}")
-    private String accessKeyId;
+    // @Value("${aws.access.key.id}")
+    // private String accessKeyId;
 
-    @Value("${aws.secret.access.key}")
-    private String secretAccessKey;
+    // @Value("${aws.secret.access.key}")
+    // private String secretAccessKey;
 
-    @Value("${aws.region}")
-    private String region;
+    // @Value("${aws.region}")
+    // private String region;
 
-    @Value("${aws.s3.bucket.name}")
-    private String bucketName;
+    // @Value("${aws.s3.bucket.name}")
+    // private String bucketName;
 
-    @Bean
-    public S3Client s3Client() {
-        validateAwsConfig();
-        return S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(
-                        StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(accessKeyId, secretAccessKey)
-                        )
-                )
-                .build();
-    }
+    // @Bean
+    // public S3Client s3Client() {
+    //     return S3Client.builder()
+    //             .region(Region.of(region))
+    //             .credentialsProvider(
+    //                     StaticCredentialsProvider.create(
+    //                             AwsBasicCredentials.create(accessKeyId, secretAccessKey)
+    //                     )
+    //             )
+    //             .build();
+    // }
 
-    @Bean
-    public S3Presigner s3Presigner() {
-        validateAwsConfig();
-        return S3Presigner.builder()
-                .region(Region.of(region))
-                .credentialsProvider(
-                        StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(accessKeyId, secretAccessKey)
-                        )
-                )
-                .build();
-    }
+    // @Bean
+    // public S3Presigner s3Presigner() {
+    //     return S3Presigner.builder()
+    //             .region(Region.of(region))
+    //             .credentialsProvider(
+    //                     StaticCredentialsProvider.create(
+    //                             AwsBasicCredentials.create(accessKeyId, secretAccessKey)
+    //                     )
+    //             )
+    //             .build();
+    // }
 
-    @Bean
-    public String bucketName() {
-        validateAwsConfig();
-        return bucketName;
-    }
-
-    private void validateAwsConfig() {
-        if (accessKeyId == null || secretAccessKey == null || region == null || bucketName == null) {
-            throw new IllegalStateException("AWS configuration properties are not set properly.");
-        }
-    }
+    // @Bean
+    // public String bucketName() {
+    //     return bucketName;
+    // }
 }
