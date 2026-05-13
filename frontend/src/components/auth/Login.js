b020707e565f37a56f0a69a8fd73a6374b16bb65
Renata <renatasch78@users.noreply.github.com>
2026-05-13 18:21:00 +0100

    Teste vercel Daniel com Renata

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -76,7 +76,7 @@ const Login = () => {
             Surf Champ - Login
           </Typography>
           <Typography variant="body2" align="center" sx={{ mb: 2 }}>
-            Teste de push na branch vercel
+            Teste de push na branch vercel - Daniel
           </Typography>
           
           {error && (
